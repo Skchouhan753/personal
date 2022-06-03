c18ec52ea9b29f2e9a3fd1437f8d937307b3e2e3
Skchouhan753 <s.k.chouhan753@gmail.com>
2022-06-03 23:59:07 +0530

    js

--- Array Of Object/SurajLal.js
+++ Array Of Object/SurajLal.js
@@ -1,13 +1,23 @@
 let data={
-    name:"suraj lal singh",
+    name:"Suraj Lal Singh",
     upperCase:function(){
-    let uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    for(let i=0;i<uppercase.length;i++){
-        for(let j=0;j<this.name.length;j++){
-            
+    let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lower="abcdefghijklmnopqrstuvwxyz";
+let result="";
+let count="nhi mila";
+    for(let i=0;i<this.name.length;i++){
+        for(let j=0;j<lower.length;j++){
+            if(this.name[i]==lower[j]){
+                result=result+upper[j];
+                count="mil gya";
+            }
         }
+        if(count=="nhi mila"){
+        result=result+this.name[i];
     }
+    }
+    console.log(result)
     }
 
 };
+    data["upperCase"]();
